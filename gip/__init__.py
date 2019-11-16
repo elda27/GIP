44529947c7f6ca6c6e0fa5a6928074bdc5f54787
elda27 <kaz.birdstick@gmail.com>
2019-11-16 17:20:01 +0800

    Change entry point

--- gip/__init__.py
+++ gip/__init__.py
@@ -4,6 +4,7 @@ from gip import converters  # NOQA
 from gip import envs  # NOQA
 from gip import functions  # NOQA
 from gip import logging  # NOQA
+from gip import version  # NOQA
+from gip import main  # NOQA
 
-
-__version__ = '0.1.0'
+__version__ = version.__version__
