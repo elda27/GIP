9cd5c40209429348179368691fcf86145bbf02e7
elda27 <kaz.birdstick@gmail.com>
2019-11-16 17:21:21 +0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Get version from main script
+__version__ = None
+
+src_dir = Path(__file__).parent.absolute()
+main_file = src_dir / 'gip' / 'version.py'
+with open(main_file, 'r') as fp:
+    exec(fp.read())
+
+fndoc = Path(src_dir) / 'README.md'
+with open(fndoc, 'r') as fp:
+    README = fp.read()
+
+setup(
+    name='gip',
+    version=__version__,
+    description='Image processing tool for console',
+    long_description=README,
+    license='MIT Licences',
+    url=https: // github.com/elda27/gip,
+    maintainer=elda27,
+    # maintainer_email=None,
+    platforms=['any'],
+    packages=[
+        'gip',
+        'gip.args',
+        'gip.converters',
+        'gip.core',
+        'gip.envs',
+        'gip.functions',
+        'gip.logging',
+        'gip.third_party',
+    ],
+    extras_require=extras_require,
+    entry_points={
+        'console_scripts': [
+            "gip = gip.main:main"
+        ]
+    },
+    python_requires='>=3.6',
+    classifiers=[
+        # (https://pypi.org/pypi?%3Aaction=list_classifiers)
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft',
+        'Operating System :: Microsoft :: MS-DOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: SunOS/Solaris',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    # keywords=None,
+)
