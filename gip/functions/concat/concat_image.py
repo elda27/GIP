7e43163464f386b8f12e1be1e53865bc9906dc0c
elda27 <kaz.birdstick@gmail.com>
2019-11-17 20:48:10 +0800

    Modify document

--- gip/functions/concat/concat_image.py
+++ gip/functions/concat/concat_image.py
@@ -2,14 +2,32 @@ from gip import core
 from gip.functions.concat.concat import Concat
 from gip.core.validate_image import ValidateImage
 import numpy as np
+from typing import List
 
 
-@core.register_function('concat_image')
+# @core.register_function('concat_image')
 @Concat._extend('concat_image_align_square', aligns=['width', 'height'])
 @Concat._extend('concat_image_align_width', aligns=['width'])
 @Concat._extend('concat_image_align_height', aligns=['height'])
 class ConcatImage(Concat):
-    def __init__(self, *args, aligns, **kwargs):
+    def __init__(self, *args, aligns: List[str] = None, **kwargs):
+        """Concat images
+
+        Args:
+            image (int, Variable): First element of images or number of images 
+                to concat from stack.
+            images (Variable): Images will be concatenated.
+            aligns (List[str]): Align mode, {'width', 'height'}.
+            stack (str, optional): Stacking method, {'horz', 'vert', 'MxN'}. 
+                About 'MxN', a shape of stacking and M or N should be digit.
+                Defaults to 'horz'.
+            mode (str, optional): Alignment size method of the 
+                difference sizes. Defaults to 'square'.
+            image_type (str, optional): Concatenation will be applied 
+                this image type. Reserved parameter. 
+                Defaults to None.
+            background (Color, optional): Padding color 
+        """
         self.aligns = aligns
         self.block_shape = None
         super().__init__(*args, aligns=aligns, **kwargs)
