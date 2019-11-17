e6d00bd5f7fb73dbd442b243a2db28cb02ee6524
elda27 <kaz.birdstick@gmail.com>
2019-11-17 17:46:43 +0800

    Fixed bag

--- gip/functions/__init__.py
+++ gip/functions/__init__.py
@@ -3,3 +3,6 @@ from gip.functions.image_reader import ImageReader  # NOQA
 from gip.functions.image_writer import ImageWriter  # NOQA
 from gip.functions.list_function import ListFunction  # NOQA
 from gip.functions.as_variable import AsVariable  # NOQA
+
+from gip.functions.concat.concat import Concat  # NOQA
+from gip.functions.concat.concat_image import ConcatImage  # NOQA
