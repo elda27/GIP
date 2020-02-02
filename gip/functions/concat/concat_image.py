c7d6e029ccaf73951426f4e045cab2185f0f304a
elda27 <kaz.birdstick@gmail.com>
2020-02-02 13:08:39 +0900

    Modify implementation of concat

--- gip/functions/concat/concat_image.py
+++ gip/functions/concat/concat_image.py
@@ -1,40 +1,37 @@
 from gip import core
 from gip.functions.concat.concat import Concat
 from gip.core.validate_image import ValidateImage
-import numpy as np
 from typing import List
+import numpy as np
 
 
 # @core.register_function('concat_image')
-@Concat._extend('concat_image_align_square', aligns=['width', 'height'])
-@Concat._extend('concat_image_align_width', aligns=['width'])
-@Concat._extend('concat_image_align_height', aligns=['height'])
+@Concat._extend()
 class ConcatImage(Concat):
-    def __init__(self, *args, aligns: List[str] = None, **kwargs):
-        """Concat images
+    align_methods = {
+        'left': lambda x, s: 0,
+        'center': lambda x, s: (s - x) // 2,
+        'right': lambda x, s: (s - x),
+    }
 
-        Args:
-            image (int, Variable): First element of images or number of images 
-                to concat from stack.
-            images (Variable): Images will be concatenated.
-            aligns (List[str]): Align mode, {'width', 'height'}.
-            stack (str, optional): Stacking method, {'horz', 'vert', 'MxN'}. 
-                About 'MxN', a shape of stacking and M or N should be digit.
-                Defaults to 'horz'.
-            mode (str, optional): Alignment size method of the 
-                difference sizes. Defaults to 'square'.
-            image_type (str, optional): Concatenation will be applied 
-                this image type. Reserved parameter. 
-                Defaults to None.
-            background (Color, optional): Padding color 
-        """
-        self.aligns = aligns
+    def __init__(self, *args, **kwargs):
+        self.__doc__ = Concat.__doc__
         self.block_shape = None
-        super().__init__(*args, aligns=aligns, **kwargs)
+        super().__init__(*args, **kwargs)
+
+        align_pos = self.align.find('|')
+        if align_pos < 0:
+            width_align = self.align
+            height_align = self.align
+        else:
+            width_align = self.align[:align_pos]
+            height_align = self.align[align_pos + 1:]
+        self.width_align_method = self.align_methods[width_align]
+        self.height_align_method = self.align_methods[height_align]
 
     def validate(self):
         block_shape = {}
-        for align in self.aligns:
+        for align in self.aligns:  # Compute maximum size of images
             block_shape.setdefault(align, []).append(
                 max([getattr(i, align) for i in self.images]))
         self.block_shape = block_shape
@@ -55,7 +52,11 @@ class ConcatImage(Concat):
             w = img.width
             h = img.height
             np.core.multiarray.copyto(
-                output.roi_image(bx * bw, by * bh, w, h).data,
+                output.roi_image(
+                    bx * bw + self.width_align_method(w, bw),
+                    by * bh + self.height_align_method(h, bh),
+                    w, h
+                ).data,
                 img.data, mode='unsafe'
             )
         return output
